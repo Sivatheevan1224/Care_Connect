d291b977d9859ef7d730ba13b2db7e699e80f043
Abbi <abbishanth1209@gmail.com>
2026-01-02 22:07:04 +0530

    feat: Add routing with react-router-dom and implement Doctors page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Home from './pages/Home'
+import DoctorsPage from './pages/DoctorsPage'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <Home />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/doctors" element={<DoctorsPage />} />
+      </Routes>
+    </Router>
   )
 }
 
